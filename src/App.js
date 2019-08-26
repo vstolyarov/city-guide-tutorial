a35ccdc7d5fbc93052085e2415dae6d978525e4c
vstolyarov <vstolyarov@yahoo.com>
2019-08-25 22:43:23 -0400

    4th

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
+        <Footer />
       </div>
       
     )
---
+++ src/components/Footer.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="mainfooter">
+      <div className="container">
+        <div className="row">
+          {/* Column1 */}
+          <div className="col-md-3 col-sm-6">
+          <h4>Lorem ipsum</h4>
+          <ul className="list-ustyled">
+            <li>Lorem ipsum</li>
+            <li>Lorem ipsum</li>
+            <li>Lorem ipsum</li>
+            <li>Lorem ipsum</li>
+          </ul>
+        </div>
+        {/* Column2 */}
+        <div className="col-md-3 col-sm-6">
+            <h4>Lorem ipsum</h4>
+          <ul className="list-ustyled">
+            <li>Lorem ipsum</li>
+            <li>Lorem ipsum</li>
+            <li>Lorem ipsum</li>
+            <li>Lorem ipsum</li>
+          </ul>
+        </div>
+        {/* Column3 */}
+        <div className="col-md-3 col-sm-6">
+            <h4>Lorem ipsum</h4>
+          <ul className="list-ustyled">
+            <li>Lorem ipsum</li>
+            <li>Lorem ipsum</li>
+            <li>Lorem ipsum</li>
+            <li>Lorem ipsum</li>
+          </ul>
+        </div>
+        <div className="col-md-3 col-sm-6">
+        <h4>Lorem ipsum</h4>
+        <ul className="list-ustyled">
+        <li>Lorem ipsum</li>
+        <li>Lorem ipsum</li>
+        <li>Lorem ipsum</li>
+        <li>Lorem ipsum</li>
+        </ul>
+        </div>
+    </div>
+  </div>
+  </div>
+  );
+}
+
+export default Footer;
