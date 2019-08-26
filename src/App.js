3e6e6a29ab174027805dd6927344ac5bad4a921f
vstolyarov <vstolyarov@yahoo.com>
2019-08-25 23:16:46 -0400

    5th

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
+        <br />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
+        recusandae consectetur quis cum quo ab porro assumenda eaque suscipit
+        alias fugit laudantium, modi quia quae. Voluptatibus explicabo officiis
+        deleniti animi, ut, optio, maiores vero culpa esse impedit quo illo
+        modi. Labore natus sunt perspiciatis! Neque!
         <Footer />
       </div>
-      
-    )
-    }
+    );
+  }
 }
-
 
 export default App;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,53 +1,85 @@
 import React from "react";
+import styled from "styled-components";
 
 function Footer() {
   return (
-    <div className="mainfooter">
-      <div className="container">
-        <div className="row">
-          {/* Column1 */}
-          <div className="col-md-3 col-sm-6">
-          <h4>Lorem ipsum</h4>
-          <ul className="list-ustyled">
-            <li>Lorem ipsum</li>
-            <li>Lorem ipsum</li>
-            <li>Lorem ipsum</li>
-            <li>Lorem ipsum</li>
-          </ul>
+    <FooterContainer>
+      <div className="footer-middle">
+        <div className="container">
+          <div className="row">
+            {/* Column1 */}
+            <div className="col-md-3 col-sm-6">
+              <h4>Lorem ipsum</h4>
+              <ul className="list-ustyled">
+                <li>Lorem ipsum Address</li>
+                <li>Lorem ipsum Phone</li>
+                <li>Lorem ipsum</li>
+                <li>Lorem ipsum</li>
+              </ul>
+            </div>
+            {/* Column2 */}
+            <div className="col-md-3 col-sm-6">
+              <h4>Lorem ipsum</h4>
+              <ul className="list-ustyled">
+                <li><a href="/">Lorem ipsum</a></li>
+                <li><a href="/">Lorem ipsum</a></li>
+                <li><a href="/">Lorem ipsum</a></li>
+                <li><a href="/">Lorem ipsum</a></li>
+              </ul>
+            </div>
+            {/* Column3 */}
+            <div className="col-md-3 col-sm-6">
+              <h4>Lorem ipsum</h4>
+              <ul className="list-ustyled">
+                <li><a href="/">Lorem ipsum</a></li>
+                <li><a href="/">Lorem ipsum</a></li>
+                <li><a href="/">Lorem ipsum</a></li>
+                <li><a href="/">Lorem ipsum</a></li>
+              </ul>
+            </div>
+            <div className="col-md-3 col-sm-6">
+              <h4>Lorem ipsum</h4>
+              <ul className="list-ustyled">
+                <li><a href="/">Lorem ipsum</a></li>
+                <li><a href="/">Lorem ipsum</a></li>
+                <li><a href="/">Lorem ipsum</a></li>
+                <li><a href="/">Lorem ipsum</a></li>
+              </ul>
+            </div>
+          </div>
+          {/* Footer Bottom */}
+          <div className="footer-bottom">
+            <p className="text-xs-center">
+              &copy;{new Date().getFullYear()} City Guide App - All rights
+              reserved
+            </p>
+          </div>
         </div>
-        {/* Column2 */}
-        <div className="col-md-3 col-sm-6">
-            <h4>Lorem ipsum</h4>
-          <ul className="list-ustyled">
-            <li>Lorem ipsum</li>
-            <li>Lorem ipsum</li>
-            <li>Lorem ipsum</li>
-            <li>Lorem ipsum</li>
-          </ul>
-        </div>
-        {/* Column3 */}
-        <div className="col-md-3 col-sm-6">
-            <h4>Lorem ipsum</h4>
-          <ul className="list-ustyled">
-            <li>Lorem ipsum</li>
-            <li>Lorem ipsum</li>
-            <li>Lorem ipsum</li>
-            <li>Lorem ipsum</li>
-          </ul>
-        </div>
-        <div className="col-md-3 col-sm-6">
-        <h4>Lorem ipsum</h4>
-        <ul className="list-ustyled">
-        <li>Lorem ipsum</li>
-        <li>Lorem ipsum</li>
-        <li>Lorem ipsum</li>
-        <li>Lorem ipsum</li>
-        </ul>
-        </div>
-    </div>
-  </div>
-  </div>
+      </div>
+    </FooterContainer>
   );
 }
 
 export default Footer;
+
+const FooterContainer = styled.footer`
+.footer-middle {
+    background: var(--mainDark);
+    padding-top: 3rem;
+    color: var(--mainWhite)
+    }
+
+.footer-bottom {
+    padding-top: 3rem;
+    padding-bottom: 2rem;
+
+}
+
+ul li a {
+  color: var(--mainGrey);
+}
+
+ul li a:hover {
+    color: var(--mainLightGrey);
+}
+`;
